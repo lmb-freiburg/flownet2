1d72c5206b891d9dc97d8fe075213e0afbfa9814
Nikolaus Mayer <mayern@informatik.uni-freiburg.de>
2017-09-01 15:43:38 +0200

    Pretrained KITTI and Sintel models

--- models/download-models.sh
+++ models/download-models.sh
@@ -2,3 +2,9 @@
 
 wget https://lmb.informatik.uni-freiburg.de/resources/binaries/flownet2/flownet2-models.tar.gz
 tar xvzf flownet2-models.tar.gz       
+
+wget https://lmb.informatik.uni-freiburg.de/resources/binaries/flownet2/flownet2-models-kitti.tar.gz
+tar xvzf flownet2-models-kitti.tar.gz       
+
+wget https://lmb.informatik.uni-freiburg.de/resources/binaries/flownet2/flownet2-models-sintel.tar.gz
+tar xvzf flownet2-models-sintel.tar.gz       
