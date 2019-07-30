b92e198b56b0e52e1ba0a5a98dc0e39fa5ae70cc
Nikolaus Mayer <nikolausmayer@users.noreply.github.com>
2019-07-30 09:20:09 +0200

    Update download.sh

--- data/download.sh
+++ data/download.sh
@@ -3,16 +3,16 @@
 mkdir FlyingThings3D_release
 cd FlyingThings3D_release
 
-wget http://lmb.informatik.uni-freiburg.de/data/SceneFlowDatasets_CVPR16/Release_april16/data/FlyingThings3D/raw_data/flyingthings3d__frames_cleanpass.tar
-wget http://lmb.informatik.uni-freiburg.de/data/SceneFlowDatasets_CVPR16/Release_april16/data/FlyingThings3D/derived_data/flyingthings3d__optical_flow.tar.bz2
+wget --no-check-certificate http://lmb.informatik.uni-freiburg.de/data/SceneFlowDatasets_CVPR16/Release_april16/data/FlyingThings3D/raw_data/flyingthings3d__frames_cleanpass.tar
+wget --no-check-certificate http://lmb.informatik.uni-freiburg.de/data/SceneFlowDatasets_CVPR16/Release_april16/data/FlyingThings3D/derived_data/flyingthings3d__optical_flow.tar.bz2
 
 tar xvf flyingthings3d__frames_cleanpass.tar
 tar xvf flyingthings3d__optical_flow.tar.bz2
 
 cd ..
-wget http://lmb.informatik.uni-freiburg.de/data/FlyingChairs/FlyingChairs.zip
+wget --no-check-certificate http://lmb.informatik.uni-freiburg.de/data/FlyingChairs/FlyingChairs.zip
 unzip FlyingChairs.zip
 
-wget https://lmb.informatik.uni-freiburg.de/data/FlowNet2/ChairsSDHom/ChairsSDHom.tar.gz
+wget --no-check-certificate https://lmb.informatik.uni-freiburg.de/data/FlowNet2/ChairsSDHom/ChairsSDHom.tar.gz
 tar xvzf ChairsSDHom.tar.gz
 
