469b0739a079f4307a8e07cf5101e6a8aa5d836b
Nikolaus Mayer <nikolausmayer@users.noreply.github.com>
2019-07-30 09:18:51 +0200

    Update download-models.sh

--- models/download-models.sh
+++ models/download-models.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
-wget https://lmb.informatik.uni-freiburg.de/resources/binaries/flownet2/flownet2-models.tar.gz
+wget --no-check-certificate https://lmb.informatik.uni-freiburg.de/resources/binaries/flownet2/flownet2-models.tar.gz
 tar xvzf flownet2-models.tar.gz       
 
-wget https://lmb.informatik.uni-freiburg.de/resources/binaries/flownet2/flownet2-models-kitti.tar.gz
+wget --no-check-certificate https://lmb.informatik.uni-freiburg.de/resources/binaries/flownet2/flownet2-models-kitti.tar.gz
 tar xvzf flownet2-models-kitti.tar.gz       
 
-wget https://lmb.informatik.uni-freiburg.de/resources/binaries/flownet2/flownet2-models-sintel.tar.gz
+wget --no-check-certificate https://lmb.informatik.uni-freiburg.de/resources/binaries/flownet2/flownet2-models-sintel.tar.gz
 tar xvzf flownet2-models-sintel.tar.gz       
